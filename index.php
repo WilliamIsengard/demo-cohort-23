4e9c44ca8d639fcac2be7676c9923601e42d1717
William Isengard <william.isengard@gmail.com>
2019-01-14 11:52:57 -0700

    Created an index file for my demo

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Basic Demo of project creation</title>
+	</head>
+	<body>
+		<h1>Deep Dive Fullstack Cohort</h1>
+	</body>
+</html>
